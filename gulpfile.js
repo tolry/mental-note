de04e959366b2fa87286d10495ac32b0111bf57d
Tobias Olry <tobias.olry@web.de>
2015-12-14 23:33:53 +0100

    almost working

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp        = require('gulp'),
+    concat      = require('gulp-concat'),
+    cssCompress = require('gulp-minify-css'),
+    uglify      = require('gulp-uglify')
+;
+
+var cssFiles = [
+    'node_modules/bootstrap/dist/css/bootstrap.css',
+    'node_modules/select2/dist/css/select2.css',
+    'node_modules/font-awesome/css/font-awesome.css',
+    'assets/css/loaders.css/loaders.css',
+    'assets/css/*css'
+];
+
+var jsFiles = [
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js',
+    'node_modules/select2/dist/css/select2.js',
+    'node_modules/jscroll/jquery.jscroll.js',
+    'assets/js/jquery-imageloader/jquery.imageloader.js',
+    'assets/js/jQuery.modalAjaxForm.js',
+    'assets/js/main.js'
+];
+
+var fontFiles = [
+    'node_modules/bootstrap/dist/fonts/**',
+    'node_modules/font-awesome/fonts/**'
+];
+
+gulp.task('css', function () {
+    gulp.src(cssFiles)
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('web/css/'))
+        .pipe(cssCompress())
+        .pipe(concat('all.min.css'))
+        .pipe(gulp.dest('web/css/'))
+    ;
+
+    gulp.src(['node_modules/semantic-ui-css/themes/**'])
+        .pipe(gulp.dest('web/css/themes'));
+});
+
+gulp.task('js', function () {
+    gulp.src(jsFiles)
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('web/js/'))
+        .pipe(uglify())
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest('web/js/'))
+    ;
+});
+
+gulp.task('fonts', function () {
+    gulp.src(fontFiles)
+        .pipe(gulp.dest('web/fonts/'));
+});
+
+gulp.task('watch', ['js', 'css'], function () {
+    gulp.watch('assets/css/*.css', ['css']);
+    gulp.watch('assets/js/*.js', ['js']);
+});
+
+gulp.task('default', ['js', 'css', 'fonts']);
