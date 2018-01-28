4a0f2f7eb5480d35f73565caf7b86607e55719ee
Tobias Olry <tobias.olry@web.de>
2018-01-28 15:02:06 +0100

    added test for creating a new entry

---
+++ tests/AppBundle/Tests/Controller/EntryControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AppBundle\Tests\Controller;
+
+use AppBundle\Entity\Category;
+use AppBundle\Tests\WebTestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class EntryControllerTest extends WebTestCase
+{
+    public function testCreateSuccessfull(): void
+    {
+        $client = $this->getAuthenticatedClient();
+
+        $crawler = $client->request('GET', '/entry/create.html');
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('form[name="entry"]')->count());
+
+        $form = $crawler->filter('form[name="entry"]')->form();
+        $form['entry[title]'] = 'functional test add entry';
+        $form['entry[url]'] = 'https://www.tobias-olry.de/?mental-note-functional-test=' . uniqid();
+        $form['entry[category]'] = Category::READ;
+        $form['entry[tags]'] = 'one, two, three';
+
+        $crawler = $client->submit($form);
+
+        $response = $client->getResponse();
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals('/', $response->getTargetUrl());
+    }
+
+    public function testIndexWithFilter(): void
+    {
+        $this->markTestSkipped();
+        $client = $this->getAuthenticatedClient();
+
+        $query = http_build_query(['filter' => ['tag' => 'CSS']]);
+        $crawler = $client->request('GET', "/?$query");
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('.user-dropdown')->count());
+    }
+}
