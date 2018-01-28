2833340c3c785d76322d1c8037653feb166af33a
Tobias Olry <tobias.olry@web.de>
2018-01-28 20:48:01 +0100

    fix error on empty tag string

--- tests/AppBundle/Tests/Controller/EntryControllerTest.php
+++ tests/AppBundle/Tests/Controller/EntryControllerTest.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class EntryControllerTest extends WebTestCase
 {
-    public function testCreateSuccessfull(): void
+    public function testCreate(): void
     {
         $client = $this->getAuthenticatedClient();
 
@@ -26,6 +26,30 @@ class EntryControllerTest extends WebTestCase
         $form['entry[url]'] = 'https://www.tobias-olry.de/?mental-note-functional-test=' . uniqid();
         $form['entry[category]'] = Category::READ;
         $form['entry[tags]'] = 'one, two, three';
+
+        $crawler = $client->submit($form);
+
+        $response = $client->getResponse();
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals('/', $response->getTargetUrl());
+    }
+
+    public function testCreateEmptyTags(): void
+    {
+        $client = $this->getAuthenticatedClient();
+
+        $crawler = $client->request('GET', '/entry/create.html');
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('form[name="entry"]')->count());
+
+        $form = $crawler->filter('form[name="entry"]')->form();
+        $form['entry[title]'] = 'functional test add entry';
+        $form['entry[url]'] = 'https://www.tobias-olry.de/?mental-note-functional-test=' . uniqid();
+        $form['entry[category]'] = Category::READ;
+        $form['entry[tags]'] = null;
 
         $crawler = $client->submit($form);
 
