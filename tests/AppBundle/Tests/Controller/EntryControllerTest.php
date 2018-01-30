d7b1cb78b812beed86d39a6c897d6f179599d051
Tobias Olry <tobias.olry@gmail.com>
2018-01-30 14:56:47 +0100

    cleaner tests, more tests

--- tests/AppBundle/Tests/Controller/EntryControllerTest.php
+++ tests/AppBundle/Tests/Controller/EntryControllerTest.php
@@ -6,6 +6,7 @@ namespace AppBundle\Tests\Controller;
 
 use AppBundle\Entity\Category;
 use AppBundle\Tests\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -59,16 +60,68 @@ class EntryControllerTest extends WebTestCase
         $this->assertEquals('/', $response->getTargetUrl());
     }
 
-    public function testIndexWithFilter(): void
+    public function testThumbnail(): void
     {
-        $this->markTestSkipped();
         $client = $this->getAuthenticatedClient();
+        $id = $this->createNew($client);
 
-        $query = http_build_query(['filter' => ['tag' => 'CSS']]);
-        $crawler = $client->request('GET', "/?$query");
+        $imageLink = "/thumbnails/${id}_100x100.png";
+
+        $client->request('GET', $imageLink);
+
+        $response = $client->getResponse();
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals($imageLink, $response->getTargetUrl());
+    }
+
+    public function testTogglePending(): void
+    {
+        $client = $this->getAuthenticatedClient();
+        $id = $this->createNew($client);
+
+        $crawler = $client->request('POST', "/entry/$id/toggle_pending.json");
+        $response = $client->getResponse();
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals('/', $response->getTargetUrl());
+
+        $crawler = $client->request('POST', "/entry/$id/toggle_pending.json");
+        $response = $client->getResponse();
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals('/', $response->getTargetUrl());
+    }
+
+    private function createNew(Client $client): int
+    {
+        $crawler = $client->request('GET', '/entry/create.html');
         $response = $client->getResponse();
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(1, $crawler->filter('.user-dropdown')->count());
+        $this->assertEquals(1, $crawler->filter('form[name="entry"]')->count());
+
+        $uniqueUrl = 'https://www.tobias-olry.de/?mental-note-functional-test=' . uniqid();
+
+        $form = $crawler->filter('form[name="entry"]')->form();
+        $form['entry[title]'] = 'functional test add entry';
+        $form['entry[url]'] = $uniqueUrl;
+        $form['entry[category]'] = Category::READ;
+        $form['entry[tags]'] = null;
+
+        $crawler = $client->submit($form);
+
+        $response = $client->getResponse();
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals('/', $response->getTargetUrl());
+
+        $crawler = $client->request('GET', '/');
+        $visitLink = $crawler->filter(".card>a[href='$uniqueUrl']")->first()->attr('data-link');
+        if (!preg_match('%/entry/(\d+)/visit%', $visitLink, $matches)) {
+            $this->fail("entry creation failed for $uniqueUrl, could not find ID in $visitLink");
+        }
+
+        return (int) $matches[1];
     }
 }
