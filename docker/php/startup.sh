deef12e33909cbf3da2b568f0570ec3fe9872039
Tobias Olry <tobias.olry@web.de>
2025-06-19 21:52:41 +0200

    wait for mysql

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -5,6 +5,13 @@ set -eEuo pipefail
 cd /app
 
 composer install --quiet --no-interaction --no-scripts
+
+while ! bin/console do:qu:sql -q 'CREATE DATABASE IF NOT EXISTS mental_note' >/dev/null 2>&1;
+do
+  echo WAITING FOR MYSQL AVAILIBILITY
+  sleep 3;
+done;
+
 bin/console do:mi:mi --no-interaction --quiet
 
 php-fpm
